06988f660e5b74c3dac1c2d45726ff265f1fe09a
Alex <cadraus@gmail.com>
2022-11-26 11:56:41 +0000

    clean vector iteration

--- CppPractice.cpp
+++ CppPractice.cpp
@@ -7,12 +7,20 @@ int main (int argc, char* argv[]) {
 vector <int> numbers;
 
 for (int i=0; i<10; i++) {
-    numbers.push_back(i);
+    numbers.push_back(i+100);
 }
 
-for (int k:numbers) {
-    cout << numbers[k] << " ";
+for (auto n: numbers) {
+    cout << n << "\t";
 }
+/*
+for (auto k = numbers.begin(); k != numbers.end(); k++) {
+    //cout << *k << " "; //cannot print the iterator unless you dereference it with *
+    //cout << *k << endl; //print the address of the iterator
+    //cout << &(*k) << endl; //print the address of each value
+   
+
+} */
 
     return 0;
 }
